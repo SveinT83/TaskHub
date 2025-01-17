5c5126d31caaa7113c522c6a2e2e5a5958edbd0f
Svein Tore <sveintore@tronderdata.no>
2025-01-17 09:50:45 +0100

    test3

--- modules/kbartickles/src/Providers/KbarticklesServiceProvider.php
+++ modules/kbartickles/src/Providers/KbarticklesServiceProvider.php
@@ -5,7 +5,7 @@ namespace tronderdata\kbartickles\Providers;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
 
-class KbArticklesServiceProvider extends ServiceProvider
+class KbarticklesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
@@ -20,31 +20,6 @@ class KbArticklesServiceProvider extends ServiceProvider
 
     public function register()
     {
-        // Registrering av eventuelle tjenester
+        // Registration of any services
     }
 }
-
-/*
-
-// Old version
-
-<?php
-namespace tronderdata\kbartickles\Providers;
-
-use Illuminate\Support\ServiceProvider;
-
-class KbarticklesServiceProvider extends ServiceProvider
-{
-    public function boot()
-    {
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'kbartickles');
-        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
-    }
-
-    public function register()
-    {
-        // Optional service bindings
-    }
-}
-*/
